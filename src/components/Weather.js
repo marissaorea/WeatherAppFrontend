ffff70db0a0fee61c490ad6caa218ba1b842b8d3
mguevara717 <marissa.guevara717@gmail.com>
2019-02-28 22:01:35 -0500

    added styling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,20 +1,43 @@
 import React from "react";
 
+const Weather = props => {
+  //destructure at a later time
 
-const Weather = (props) => {
-//destructure at a later time
-
-    return(
-      <div>
-        {props.city && props.country && <p>Location: {props.city}, {props.country}</p>}
-        {props.temperature && <p>Temperature: {props.temperature}</p>}
-        {props.humidity && <p>Humidity: {props.humidity}</p>}
-        {props.description && <p>Conditions: {props.description}</p>}
-        {props.error && <p>{props.error}</p>}
-      </div>
-    )
-
-
-}
+  return (
+    <div className="weather__info">
+      {props.city && props.country && (
+        <p className="weather__key">
+          Location:
+          <span>
+            {props.city}, {props.country}
+          </span>
+        </p>
+      )}
+      {props.temperature && (
+        <p className="weather__key">
+          Temperature:
+          <span>{props.temperature}</span>
+        </p>
+      )}
+      {props.humidity && (
+        <p className="weather__key">
+          Humidity:
+          <span>{props.humidity}</span>
+        </p>
+      )}
+      {props.description && (
+        <p className="weather__key">
+          Conditions:
+          <span>{props.description}</span>
+        </p>
+      )}
+      {props.error && (
+        <p className="weather__key">
+          <span>{props.error}</span>
+        </p>
+      )}
+    </div>
+  );
+};
 
 export default Weather;
